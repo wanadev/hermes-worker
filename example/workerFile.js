7ca7a9e1da2da3275065b06f9c14e0102db0ac58
Victor Serain <vserain@wanadev.fr>
2020-07-03 17:02:27 +0200

    clean code

--- example/workerFile.js
+++ example/workerFile.js
@@ -1,39 +1,40 @@
 /* eslint-disable no-undef */
 
 // eslint-disable-next-line no-unused-vars
-// const workerFunction = () => {
-// This code is excuted in worker
+const workerFunction = () => {
+    // This code is excuted in worker
+    console.log("Worker instance " + hermes.config.threadInstance + " is started");
 
-if (hermes.config.threadInstance === 0) console.log("testScript.test > ", test);
+    if (hermes.config.threadInstance === 0) console.log("testScript.test > ", test);
 
-function fibo(n) {
-    if (n === 0 || n === 1) {
-        return 1;
+    function fibo(n) {
+        if (n === 0 || n === 1) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
     }
-    return fibo(n - 1) + fibo(n - 2);
-}
 
-function add(a, b) {
-    return a + b;
-}
+    function add(a, b) {
+        return a + b;
+    }
 
-hermes.on("add", add);
+    hermes.on("add", add);
 
-hermes.on("fibo", (number) => {
-    console.log("Fibo in worker instance " + hermes.config.threadInstance);
-    return fibo(number);
-});
+    hermes.on("fibo", (number) => {
+        console.log("Fibo in worker instance " + hermes.config.threadInstance);
+        return fibo(number);
+    });
 
-hermes.on("addVector2", (vectorA, vectorB) => vectorA.add(vectorB));
+    hermes.on("addVector2", (vectorA, vectorB) => vectorA.add(vectorB));
 
-hermes.on("wait", number => new Promise((resolve) => {
-    setTimeout(() => {
-        resolve({
-            data: "your data async",
-            message: "end",
-        });
-    }, number);
-}), { type: "async" });
+    hermes.on("wait", number => new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data: "your data async",
+                message: "end",
+            });
+        }, number);
+    }), { type: "async" });
 
-hermes.ready();
-// };
+    hermes.ready();
+};
